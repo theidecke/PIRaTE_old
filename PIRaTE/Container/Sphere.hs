a16f904cec48c34170767544737b0212572198c2
Thies Heidecke <theidecke@astrophysik.uni-kiel.de>
2009-11-12 19:54:33 +0100

    some minor tweaks in to PIRaTE.Container.Sphere

--- PIRaTE/Container/Sphere.hs
+++ PIRaTE/Container/Sphere.hs
@@ -22,26 +22,25 @@ module PIRaTE.Container.Sphere where
   instance Confineable Sphere where
     contains (Sphere c r) p = normsq (p - c) <= r*r
     {-# INLINE contains #-}
-
+    
+    -- assuming direction is normalized
     intersectedBy (Sphere center radius) (Ray origin (Direction direction)) = 
       let offset = origin - center
           oo = offset `vdot` offset
           od = offset `vdot` direction
-          dd = direction `vdot` direction
-          discriminant = od*od - dd*(oo - radius*radius)
+          discriminant = od*od - oo + radius*radius
       in if discriminant <= 0 
           then []
-          else let ddinv = 1 / dd
-                   alpha0 = ddinv * (-od)
-                   alphadelta = ddinv * sqrt discriminant
+          else let alpha0 = -od
+                   alphadelta = sqrt discriminant
                    alphas = (alpha0 - alphadelta,
                              alpha0 + alphadelta)
                in [alphas]
 
   instance Sampleable Sphere Point where
     probabilityDensityOf s@(Sphere center radius) p
-      | not (s `contains` p) = 0
-      | otherwise            = 3 / (4*pi*(radius^3))
+      | s `contains` p = 3 / (4*pi*(radius^3))
+      | otherwise      = 0
     {-# INLINE probabilityDensityOf #-}
     randomSampleFrom     (Sphere center radius) g =  do
       unitpoint <- randomPointInUnitSphere g
